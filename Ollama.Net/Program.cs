b39c87f5e114ab3bd18f918e9b55c0477940a2fc
Gabriel Costal Fogo <gabrielcostal164@gmail.com>
2026-02-08 14:13:51 -0300

    first commit

---
+++ Ollama.Net/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.AI;
+using OllamaSharp;
+
+var uri = new Uri($"http://localhost:11434/");
+const string defaultModel = "llama3:latest";
+
+IChatClient chat = new OllamaApiClient(uri, defaultModel);
+
+List<ChatMessage> chatHistory = [];
+
+while (true)
+{
+    Console.Write("Você: ");
+    
+    var enterText = Console.ReadLine();
+    
+    if (enterText?.ToLower() == "exit")
+        break;
+    
+    if(string.IsNullOrWhiteSpace(enterText))
+        continue;
+    
+    var userMessage = new ChatMessage(ChatRole.User, enterText);
+    
+    chatHistory.Add(userMessage);
+    
+    Console.Write("Assistente:");
+
+    var assistentMessageText = "";
+
+    await foreach (var messageChunk in chat.GetStreamingResponseAsync(chatHistory))
+    {
+        Console.Write(messageChunk);
+        assistentMessageText += messageChunk;      
+    }
+
+    var assistentMesssage = new ChatMessage(ChatRole.Assistant, assistentMessageText);
+    
+    chatHistory.Add(assistentMesssage);
+    
+    Console.WriteLine();
+    Console.WriteLine();
+}
